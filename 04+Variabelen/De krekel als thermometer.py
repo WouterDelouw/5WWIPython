81b9dfb718ef68b2a0327d48c484299f433d3a26
Wouter Delouw <wouter.delouw@sgsintpaulus.eu>
2019-09-24 18:35:17 +0200

    Github testen

---
+++ 04+Variabelen/De krekel als thermometer.py
@@ -0,0 +1,11 @@
+# invoer
+t = int(input('tjirps: '))
+
+#berekening
+
+f = 50 + (t - 40) / 4
+c = 10 + (t - 40) / 7
+
+# uitvoer
+print('temperatuur (Fahrenheit):', f)
+print('temperatuur (Celsius):', c)
