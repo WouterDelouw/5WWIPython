b74d14451f03ca111026e3201f3858de9fc770df
Wouter Delouw <wouter.delouw@sgsintpaulus.eu>
2019-09-25 16:52:15 +0200

    Github testen

--- 04+Variabelen/Graansilo vullen.py
+++ 04+Variabelen/Graansilo vullen.py
@@ -1,9 +1,18 @@
 # invoer
-b = input('breedte: ')
-l = input('lengte ')
-c = input('kubieke meter: ')
-r = input('straal: ')
-h = input('hoogte: ')
+b = float((input('breedte: ')))
+l = float((input('lengte: ')))
+c = float((input('kubieke meter: ')))
+r = float((input('straal: ')))
+h = float((input('hoogte: ')))
+pi = 3.14159265359
 
 # berekeningen
-aantal_silos = (b * l)
+m = float((b * l * c) / 10000)
+een_silo = float(r**2 * pi * h)
+aantal_silos = (m // een_silo) + 1
+hoogte_silo = float((m % een_silo) / (r**2 * pi))
+
+# uitvoer
+print(int(aantal_silos))
+print(hoogte_silo)
+
